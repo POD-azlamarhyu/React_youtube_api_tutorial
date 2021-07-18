69a91a32943d1d82f2753a094d85e261b71cb134
jikobouei-capital <adrague.ggoch@gmail.com>
2021-07-18 17:19:06 +0900

    add footer

---
+++ src/components/dist/Footer.js
@@ -0,0 +1,11 @@
+"use strict";
+exports.__esModule = true;
+var react_1 = require("react");
+var Footer = function () {
+    return (react_1["default"].createElement("div", { className: "footer has-background-primary" },
+        react_1["default"].createElement("div", { className: "content has-text-centered" },
+            react_1["default"].createElement("p", null,
+                react_1["default"].createElement("strong", null, "YouTube API test"),
+                "C.I. Academy 7/19/2021"))));
+};
+exports["default"] = Footer;
