65ab47a6ccbc5baa1e50afff5c8c97336f7adaf3
jikobouei-capital <adrague.ggoch@gmail.com>
2021-07-18 17:11:29 +0900

    add search text form

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import 'bulma/css/bulma.css';
 import React from 'react';
 import Header from './components/Header';
 import Movie from './components/Movie';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <>
     <Header />
     <Movie />
+    <Footer />
     </>
   );
 }
